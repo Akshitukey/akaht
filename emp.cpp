d0b2481fa94b737e53a617e41286a9ec53b45a97
Akshitukey <100211996+Akshitukey@users.noreply.github.com>
2022-02-26 13:34:08 -0500

    Add files via upload

---
+++ emp.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+ 
+/*structure declaration*/
+struct employee{
+    char    name[30];
+    int     empId;
+    float   salary;
+};
+ 
+int main()
+{
+    /*declare structure variable*/
+    struct employee emp;
+     
+    /*read employee details*/
+    printf("\nEnter details :\n");
+    printf("Name ?:akashit");          gets(emp.name);
+    printf("ID ?: 1");            scanf("%d",&emp.empId);
+    printf("Salary ?:1500");        scanf("%f",&emp.salary);
+     
+    /*print employee details*/
+    printf("\nEntered detail is:");
+    printf("Name:sager %s"   ,emp.name);
+    printf("Id:2 %d"     ,emp.empId);
+    printf("Salary:1500 %f\n",emp.salary);
+    return 0;
+   
+     /*read employee details*/
+    printf("\nEnter details :\n");
+    printf("Name ?:sachin");          gets(emp.name);
+    printf("ID ?: 3");            scanf("%d",&emp.empId);
+    printf("Salary ?:1500");      scanf("%f",&emp.salary);
+
+  
+
+
+
+}
